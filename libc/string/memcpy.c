22048712c90d5d268fc5bc0e038a375a00a739fe
Josh Gray <gray.josh@gmail.com>
2019-12-31 06:38:18 +0100

    Build libc/libk

--- libc/string/memcpy.c
+++ libc/string/memcpy.c
@@ -7,4 +7,5 @@ void* memcpy(void* dest, const void* src, size_t count)
     const uint8_t* sp = (const uint8_t*)src;
     uint8_t* dp = (uint8_t*)dest;
     for (; count != 0; count--) *dp++ = *sp++;
+    return dest;
 }
